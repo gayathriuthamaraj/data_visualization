5dc0be017fb1b748ad04febce88699f362e35748
gayathriuthamaraj <144335463+gayathriuthamaraj@users.noreply.github.com>
2025-11-03 15:09:09 +0530

    done

--- study_topics/main.py
+++ study_topics/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import crud
 
 app = FastAPI()
 
+items = []
+
+class Review(BaseModel):
+    id: int
+    content: str
+    score: int
+
 @app.get("/")
-def read_root():
-    return {"message" : "Hello World"}
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/reviews")
+def review_print():
+    return items
+
+@app.get("/hello")
+def hello(name: str = "May"):
+    return {"message" : f"Hello {name}"}
+
+@app.post("/reviews")
+def post_review(review: Review):
+    items.append(review)
+    return "review posted"
